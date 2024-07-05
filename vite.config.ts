f55a975cbffba495bd693fa28027078677083809
Chris <nzeanoruec@gmail.com>
2024-07-05 11:20:48 -0400

    Modified the vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,32 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		// proxy: {
-		// 	"/users/register": {
-		// 		target: "http://localhost:8080",
-		// 		changeOrigin: true,
-		// 		secure: false,
-		// 	},
-		// 	"/users/login": {
-		// 		target: "http://localhost:8080",
-		// 		changeOrigin: true,
-		// 		secure: false,
-		// 	},
-		// 	"/products/add": {
-		// 		target: "http://localhost:8080",
-		// 		changeOrigin: true,
-		// 		secure: false,
-		// 	},
-		// 	"/products/views": {
-		// 		target: "http://localhost:8080",
-		// 		changeOrigin: true,
-		// 		secure: false,
-		// 	},
-		// },
-	},
 	build: {
-		outDir: "./dist", // Specify your desired output directory
+		outDir: "dist", // Specify your desired output directory
 		sourcemap: true,
 	},
 });
